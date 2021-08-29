02eecfb498ebcc7ae316e55e252b58fa37c2d355
itdtin <roman.itdtin@gmail.com>
2021-08-30 00:48:43 +0200

    fix

--- always_on.py
+++ always_on.py
@@ -1,7 +1,13 @@
 from ranking import Ranking
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from app import db
 
 CMC_API_TOKEN = "2229a7b0-ebf1-403f-8470-7c32d0feefa2"
 CMC_BASE_URL = "https://pro-api.coinmarketcap.com/"
 
-r = Ranking(CMC_BASE_URL, CMC_API_TOKEN)
-r.fill_cmc_data()
+if __name__ == "__main__":
+    r = Ranking(CMC_BASE_URL, CMC_API_TOKEN, db)
+    sched = BackgroundScheduler(daemon=True, timezone="UTC")
+    sched.add_job(r.fill_cmc_data(), "interval", days=1)
+    sched.start()
