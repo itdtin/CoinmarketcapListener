e5429c1da9e624d9e2a0915db335df97e4ebc864
itdtin <roman.itdtin@gmail.com>
2021-08-29 16:24:58 +0200

    fix

--- app.py
+++ app.py
@@ -71,6 +71,9 @@ def respond():
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
+    elif text == "5 дней":
+        data = rank_listener.get_top_gainers(days=5)
+        bot.sendMessage(chat_id=chat_id, text=data)
     else:
         try:
             # clear the message we got from any non alphabets
