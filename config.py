dc98f10f37ea8f34397dfb19ce038fc8521bf240
itdtin <roman.itdtin@gmail.com>
2021-09-02 14:14:54 +0200

    fix

--- config.py
+++ config.py
@@ -24,7 +24,6 @@ class DevelopmentConfig(Config):
     DEBUG = True
     DATABASE_NAME = "ranklistener.sqlite"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    print(SQLALCHEMY_DATABASE_URI)
     # SQLALCHEMY_DATABASE_URI = f"sqlite:///{path}/{DATABASE_NAME}"
     CMC_API_TOKEN = os.environ.get("CMC_API_TOKEN")
     CMC_BASE_URL = "https://pro-api.coinmarketcap.com/"
@@ -36,4 +35,14 @@ class DevelopmentConfig(Config):
 
 
 class TestingConfig(Config):
-    TESTING = True
+    DEBUG = True
+    DATABASE_NAME = "ranklistener.sqlite"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    # SQLALCHEMY_DATABASE_URI = f"sqlite:///{path}/{DATABASE_NAME}"
+    CMC_API_TOKEN = os.environ.get("CMC_API_TOKEN")
+    CMC_BASE_URL = "https://pro-api.coinmarketcap.com/"
+
+    TG_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+    TG_WEBHOOK = "https://cmclistener.herokuapp.com/"
+
+    RESULT_COUNT = 10
