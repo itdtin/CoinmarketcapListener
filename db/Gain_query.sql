5880543bdeccc3a950bdf19ec0766e21abd1e53c
itdtin <roman.itdtin@gmail.com>
2021-10-02 10:39:03 +0200

    fix new test1

--- db/Gain_query.sql
+++ db/Gain_query.sql
@@ -6,3 +6,7 @@ first_value(rh.cmc_rank) over(partition by rh.cmc_id order by rh.last_update) fi
 last_value(rh.cmc_rank) over(partition by rh.cmc_id order by rh.last_update) last_value,
 row_number() over(partition by rh.cmc_id order by rh.last_update desc) seq
 from rank_historical rh  where rh.last_update between '2021-08-24' and '2021-08-31') a left join currencies c on a.cmc_id=c.id where seq = 1 and gain != 0 order by gain desc limit 100
+
+
+
+amdn aksndaljkslklkamsdlak
